4aa9a77f92bc1e8a4bd230ec878906c9c99e5c65
Jacks27 <simon.jackson.kariuki@gmail.com>
2022-11-27 16:22:45 +0300

    Add bootsstrap Nav

--- shambapro/src/app/app-routing.module.ts
+++ shambapro/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponentComponent } from './login-component/login-component.component';
+const routes: Routes = [
+  {path:'login', component: LoginComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
