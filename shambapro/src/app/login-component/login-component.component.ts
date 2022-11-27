0ebb45553faf3f0356b9458d051c85d0973a9c84
Jacks27 <simon.jackson.kariuki@gmail.com>
2022-11-27 19:54:50 +0300

    validate login form inputs

--- shambapro/src/app/login-component/login-component.component.ts
+++ shambapro/src/app/login-component/login-component.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login-component',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-component.component.css']
 })
 export class LoginComponentComponent {
+  get email(){
+    return this.lgform?.get('email')
+  }
+  get password(){
+    return this.lgform?.get('password')
+  }
+  constructor(private fmb:FormBuilder){}
+  lgform=this.fmb.group({
+      email:['',[Validators.required, Validators.email]],
+      password:['',Validators.required]
+    });
 
 }
